717bcd599aea154c95987f4c23861d8518b31a48
Lorenzo <lorenzo@Mac.local>
2025-07-10 11:59:16 +0200

    Fix - Vercel - Var

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['picsum.photos', 'www.silanpromozioni.com'],
+  },
   env: {
-    NEXT_PUBLIC_ENV:
-      process.env.NEXT_PUBLIC_ENV || process.env.VERCEL_ENV || 'Pro',
-    NEXT_PUBLIC_GIT_COMMIT_REF:
-      process.env.NEXT_PUBLIC_GIT_COMMIT_REF || process.env.VERCEL_GIT_COMMIT_REF,
+    NEXT_PUBLIC_ENV: process.env.VERCEL_ENV || 'Loc',
+    NEXT_PUBLIC_GIT_COMMIT_REF: process.env.VERCEL_GIT_COMMIT_REF || 'local',
     NEXT_PUBLIC_GIT_COMMIT_SHA:
-      process.env.NEXT_PUBLIC_GIT_COMMIT_SHA ||
-      process.env.VERCEL_GIT_COMMIT_SHA?.substring(0, 7) ||
-      'unknown',
+      process.env.VERCEL_GIT_COMMIT_SHA?.substring(0, 7) || 'local',
   },
 };
 
