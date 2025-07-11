cb5b6b5e7847c83958074e0e0f2ae3882ef9f045
Lorenzo <lorenzo@Mac.local>
2025-07-11 13:03:30 +0200

    Fix - Versioning

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
-import type { NextConfig } from "next";
+import 'dotenv/config' // <-- importa .env.local esplicitamente
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -6,11 +7,10 @@ const nextConfig: NextConfig = {
     domains: ['picsum.photos', 'www.silanpromozioni.com'],
   },
   env: {
-    NEXT_PUBLIC_ENV: process.env.VERCEL_ENV || 'Loc',
-    NEXT_PUBLIC_GIT_COMMIT_REF: process.env.VERCEL_GIT_COMMIT_REF || 'local',
-    NEXT_PUBLIC_GIT_COMMIT_SHA:
-      process.env.VERCEL_GIT_COMMIT_SHA?.substring(0, 7) || 'local',
+    NEXT_PUBLIC_ENV: process.env.VERCEL_ENV || process.env.NEXT_PUBLIC_ENV || 'Loc',
+    NEXT_PUBLIC_GIT_COMMIT_REF: process.env.VERCEL_GIT_COMMIT_REF || process.env.NEXT_PUBLIC_GIT_COMMIT_REF || 'local',
+    NEXT_PUBLIC_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA?.substring(0, 7) || process.env.NEXT_PUBLIC_GIT_COMMIT_SHA || 'local',
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
